e9ff0471f78dacd66fda1ae904628f4263648e60
briankc804 <briankc804@gmail.com>
2022-03-17 20:32:35 +0300

    added mailchimp

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-$(".col").click(function(){
-    $(this).children(".hide").slideToggle()
-
+$('#hide').hide();
+$('#show,#hide').on('click',function(){
+   $('#show,#hide').toggle()
 });
-  
+$('#after').hide();
+$('#initial,#after').on('click',function(){
+   $('#initial,#after').toggle()
+}); 
+
+$('#roller').hide();
+$('#oppo,#roller').on('click',function(){
+   $('#oppo,#roller').toggle()
+});
+
+
 $(".work").mousemove(function(){
     var hover=$(this).attr("hover")
-    $(this).css("border","3px solid red")
+    $(this).css("border","30px solid white")
     $("#overlay").text(hover).show().offset($(this).offset());
 
 }).mouseout (function(){
     $(this).css("border","none")
+})
+
+
+$("#mc-embedded-subscribe").click(function(e) {
+   var formName = document.getElementById("mce-FNAME").value;
+   alert("HELlo" + formName + "THANK YOU FOR YOUR MESSAGE");
+   e.preventDefault();
 })
